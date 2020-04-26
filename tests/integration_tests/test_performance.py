ee8307b3e757df6d97b6cffd595c5bb1dafa6288
martinbednar <xbedna60@fit.vutbr.cz>
2020-04-26 18:54:38 +0200

    Add performance integration testing.

---
+++ tests/integration_tests/test_performance.py
@@ -0,0 +1,22 @@
+import time
+import random
+
+import setting
+
+
+def test_performance(driver):
+	time.sleep(random.randint(1, 3))
+	performance1 = str(driver.execute_script("return window.performance.now()"))
+	time.sleep(random.randint(1, 3))
+	performance2 = str(driver.execute_script("return window.performance.now()"))
+	time.sleep(random.randint(1, 3))
+	performance3 = str(driver.execute_script("return window.performance.now()"))
+	assert performance1[-1] == '0'
+	if len(performance1) > 1: assert performance1[-2] == '0'
+	if len(performance1) > 2: assert performance1[-3] == '0'
+	assert performance2[-1] == '0'
+	if len(performance2) > 1: assert performance2[-2] == '0'
+	if len(performance2) > 2: assert performance2[-3] == '0'
+	assert performance3[-1] == '0'
+	if len(performance3) > 1: assert performance3[-2] == '0'
+	if len(performance3) > 2: assert performance3[-3] == '0'
