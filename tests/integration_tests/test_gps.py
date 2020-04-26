fd9cf2346bf5cf755852f26bacabecf2b46fcb95
martinbednar <xbedna60@fit.vutbr.cz>
2020-04-26 21:51:45 +0200

    Experiments with location (GPS) integration testing.

---
+++ tests/integration_tests/test_gps.py
@@ -0,0 +1,11 @@
+import setting
+
+
+def test_accuracy(driver):
+	device_memory = driver.execute_script("return window.navigator.deviceMemory")
+	assert device_memory == setting.expected_values.device.deviceMemory
+
+
+def test_hardware_concurrency(driver):
+	hardware_concurrency = driver.execute_script("return window.navigator.hardwareConcurrency")
+	assert hardware_concurrency == setting.expected_values.device.hardwareConcurrency
