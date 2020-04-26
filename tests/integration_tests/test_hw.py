ffcb9f851e32d1fcb496688ede32e7196fc0915e
martinbednar <xbedna60@fit.vutbr.cz>
2020-04-26 09:04:38 +0200

    Add experimental userAgent and hw tests.

--- tests/integration_tests/test_hw.py
+++ tests/integration_tests/test_hw.py
@@ -4,7 +4,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+import expected_values
+
 
 def test_a2(driver):
-	js_url = driver.execute_script("return window.navigator.userAgent")
-	assert True
+	js_url = driver.execute_script("return window.navigator.appVersion")
+	assert js_url == expected_values.expected_values.navigator.appVersion
