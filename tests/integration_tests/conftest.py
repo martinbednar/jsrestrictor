d90603920acb799d67ca508553a17af0814f886a
martinbednar <xbedna60@fit.vutbr.cz>
2020-04-26 18:19:22 +0200

    Set jsr to level 3 in the beggining of integration testing.

--- tests/integration_tests/conftest.py
+++ tests/integration_tests/conftest.py
@@ -6,6 +6,11 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
+def set_jsr_to_level(driver, level):
+	driver.get('chrome-extension://ammoloihpcbognfddfjcljgembpibcmb/options.html')
+	driver.find_element_by_id('level-' + str(level)).click()
+	driver.get('https://polcak.github.io/jsrestrictor/')
+
 @pytest.fixture(scope="session",autouse=True)
 def driver(request):
 	executable_path = "G:\\My Drive\\FIT\\4_semestr_MIS\\DIP\\Selenium\\SeleniumWebDriverJava\\WebDrivers\\chromedriver.exe"
@@ -16,5 +21,6 @@ def driver(request):
 	options.add_argument("user-data-dir=C:\\Users\\Martin\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 1")
 	
 	driver = webdriver.Chrome(executable_path=executable_path, options=options)
+	set_jsr_to_level(driver, level=3)
 	yield driver
 	driver.quit()
