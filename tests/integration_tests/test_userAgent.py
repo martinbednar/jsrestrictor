27393c959918bf98b2613e52d3e7eb1511ffca2e
martinbednar <xbedna60@fit.vutbr.cz>
2020-04-26 09:04:38 +0200

    Add experimental userAgent and hw tests.

---
+++ tests/integration_tests/test_userAgent.py
@@ -0,0 +1,47 @@
+import os
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+import expected_values
+
+
+def test_user_agent(driver):
+	user_agent = driver.execute_script("return window.navigator.userAgent")
+	assert user_agent == expected_values.expected_values.navigator.userAgent
+
+
+def test_app_version(driver):
+	app_version = driver.execute_script("return window.navigator.appVersion")
+	assert app_version == expected_values.expected_values.navigator.app_version
+
+
+def test_platform(driver):
+	platform = driver.execute_script("return window.navigator.platform")
+	assert platform == expected_values.expected_values.navigator.platform
+
+
+def test_vendor(driver):
+	vendor = driver.execute_script("return window.navigator.vendor")
+	assert vendor == expected_values.expected_values.navigator.vendor
+
+
+def test_language(driver):
+	language = driver.execute_script("return window.navigator.language")
+	assert language == expected_values.expected_values.navigator.language
+
+
+def test_languages(driver):
+	languages = driver.execute_script("return window.navigator.languages")
+	assert languages == expected_values.expected_values.navigator.languages
+
+
+def test_cookie_enabled(driver):
+	cookie_enabled = driver.execute_script("return window.navigator.cookieEnabled")
+	assert cookie_enabled == expected_values.expected_values.navigator.cookieEnabled
+
+
+def test_do_not_track(driver):
+	do_not_track = driver.execute_script("return window.navigator.doNotTrack")
+	assert do_not_track == expected_values.expected_values.navigator.doNotTrack
