ce91e99a3b246f78bcb1c8330f40868263d3585b
martinbednar <xbedna60@fit.vutbr.cz>
2020-04-27 12:52:12 +0200

    Add intergration tests for gps.

--- tests/integration_tests/test_gps.py
+++ tests/integration_tests/test_gps.py
@@ -1,11 +1,50 @@
 import time
+import pytest
 
 import setting
 
 
-def test_accuracy(driver):
+@pytest.fixture(scope='module',autouse=True)
+def position(driver):
 	show_gps_button = driver.find_element_by_xpath("//button[text()='Show GPS data']")
 	show_gps_button.click()
-	time.sleep(2)
-	print(driver.find_element_by_xpath("//div[@id='placeToWriteGPSDetails']").text)
-	assert True
+	time.sleep(1)
+	location = driver.find_element_by_xpath("//div[@id='placeToWriteGPSDetails']").text
+	location = location.replace(" ", "").split()
+	myDict = {}
+	for property in location:
+		property = property.split(':')
+		myDict[property[0].lower()] = property[1]
+	return myDict
+
+
+def test_accuracy(position):
+	assert position['accuracy'] == '0'
+
+
+def test_altitude(position):
+	assert position['altitude'] == '0'
+
+
+def test_altitudeaccurac(position):
+	assert position['altitudeaccurac'] == '0'
+
+
+def test_heading(position):
+	assert position['heading'] == '0'
+
+
+def test_latitude(position):
+	assert position['latitude'] == '0'
+
+
+def test_longitude(position):
+	assert position['longitude'] == '0'
+
+
+def test_speed(position):
+	assert position['speed'] == '0'
+
+
+def test_timestamp(position):
+	assert position['timestamp'] == '0'
