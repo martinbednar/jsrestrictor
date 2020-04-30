61a71a8bddecbe77cb1d23a915a4c75ee45ec540
martinbednar <xbedna60@fit.vutbr.cz>
2020-04-30 15:45:14 +0200

    Add parametr jsr_level on pytest start.

--- tests/integration_tests/conftest.py
+++ tests/integration_tests/conftest.py
@@ -4,8 +4,18 @@ from browser_type import BrowserType
 from browser import Browser
 
 
+def pytest_addoption(parser):
+    parser.addoption("--jsr_level", action="store", default="2")
+
+
 @pytest.fixture(scope="session", autouse=True)
-def driver():
+def jsr_level(pytestconfig):
+    return pytestconfig.getoption("jsr_level")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def driver(jsr_level):
+    print(jsr_level)
     browser = Browser(type=BrowserType.CHROME)
     browser.jsr_level = 3
     yield browser.driver
