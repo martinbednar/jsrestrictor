2e0054ab3a6a20caf9b480fb32951e479b925a65
martinbednar <xbedna60@fit.vutbr.cz>
2020-04-30 14:14:07 +0200

    Define tested values in separated class.

--- tests/integration_tests/conftest.py
+++ tests/integration_tests/conftest.py
@@ -1,12 +1,12 @@
 import pytest
 
-from Browser_type import BrowserType
-from Browser import Browser
+from browser_type import BrowserType
+from browser import Browser
 
 
 @pytest.fixture(scope="session", autouse=True)
 def driver():
-    browser = Browser(type=BrowserType.FIREFOX)
+    browser = Browser(type=BrowserType.CHROME)
     browser.jsr_level = 3
     yield browser.driver
     browser.driver.quit()
