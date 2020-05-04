a7129f8d3866efb72be8668953d924116154e4e4
martinbednar <xbedna60@fit.vutbr.cz>
2020-05-04 16:26:04 +0200

    Change Config class to static.

--- tests/integration_tests/start.py
+++ tests/integration_tests/start.py
@@ -2,12 +2,12 @@ import pytest
 
 from web_browser import Browser
 from web_browser_type import BrowserType
-from configuration import config
+from configuration import Config
 
 
-for browser_type in config.browsers:
+for browser_type in config.tested_browsers:
     my_browser = Browser(browser_type)
-    for jsr_level in config.jsr_levels:
+    for jsr_level in config.tested_jsr_levels:
         my_browser.jsr_level = jsr_level
         pytest.main()
     my_browser.quit()
