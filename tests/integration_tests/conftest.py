db35ce48b169e5087b29668218e2dd1d70488936
martinbednar <xbedna60@fit.vutbr.cz>
2020-05-04 16:49:21 +0200

    Create shared web browser.

--- tests/integration_tests/conftest.py
+++ tests/integration_tests/conftest.py
@@ -1,22 +1,24 @@
 import pytest
 import importlib
-import web_browser
+from web_browser_shared import shared_browser
 import values_expected
 
 
 @pytest.fixture(scope="session", autouse=True)
 def browser():
-    importlib.reload(web_browser)
-    return web_browser.browser
+    #importlib.reload(web_browser_shared)
+    return shared_browser
 
 
 @pytest.fixture(scope="session", autouse=True)
 def expected():
-    if web_browser.jsr_level == 0:
+    #importlib.reload(web_browser_shared)
+    print(shared_browser)
+    if shared_browser.jsr_level == 0:
         return values_expected.level0
-    elif web_browser.jsr_level == 1:
+    elif shared_browser.jsr_level == 1:
         return values_expected.level1
-    elif web_browser.jsr_level == 2:
+    elif shared_browser.jsr_level == 2:
         return values_expected.level2
-    elif web_browser.jsr_level == 3:
+    elif shared_browser.jsr_level == 3:
         return values_expected.level3
