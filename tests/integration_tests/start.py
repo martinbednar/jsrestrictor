648b4320e7c9072480a6d4e0f5aa1a03b7e84c99
martinbednar <xbedna60@fit.vutbr.cz>
2020-05-04 16:33:23 +0200

    Rename static class Config.

--- tests/integration_tests/start.py
+++ tests/integration_tests/start.py
@@ -5,9 +5,9 @@ from web_browser_type import BrowserType
 from configuration import Config
 
 
-for browser_type in config.tested_browsers:
+for browser_type in Config.tested_browsers:
     my_browser = Browser(browser_type)
-    for jsr_level in config.tested_jsr_levels:
+    for jsr_level in Config.tested_jsr_levels:
         my_browser.jsr_level = jsr_level
         pytest.main()
     my_browser.quit()
