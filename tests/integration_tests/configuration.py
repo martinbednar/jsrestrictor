8c50048ca84f4f3589f9531b7606e16d757644a8
martinbednar <xbedna60@fit.vutbr.cz>
2020-05-04 16:26:04 +0200

    Change Config class to static.

--- tests/integration_tests/configuration.py
+++ tests/integration_tests/configuration.py
@@ -2,9 +2,5 @@ from web_browser_type import BrowserType
 
 
 class Config:
-    def __init__(self):
-        self.browsers = [BrowserType.CHROME, BrowserType.FIREFOX]
-        self.jsr_levels = [0, 1, 2, 3]
-
-
-config = Config()
+    tested_browsers = [BrowserType.CHROME, BrowserType.FIREFOX]
+    tested_jsr_levels = [0, 1, 2, 3]
