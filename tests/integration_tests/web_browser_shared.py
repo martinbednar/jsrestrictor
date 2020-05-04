3a1ca04b55e757e4a5f07d98701930541363c888
martinbednar <xbedna60@fit.vutbr.cz>
2020-05-04 17:52:36 +0200

    Create and use getter for shared browser.

--- tests/integration_tests/web_browser_shared.py
+++ tests/integration_tests/web_browser_shared.py
@@ -1,14 +1,14 @@
 from web_browser_type import BrowserType
 from web_browser import Browser
 
-shared_browser = None
+_shared_browser = None
 
 
 def set_shared_browser(browser):
-    global shared_browser
-    shared_browser = browser
+    global _shared_browser
+    _shared_browser = browser
 
 
 def get_shared_browser():
-    global shared_browser
-    return shared_browser
+    global _shared_browser
+    return _shared_browser
