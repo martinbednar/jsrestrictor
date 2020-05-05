e258db0d1dd09caeadda9fdcab762bc8970d0a58
martinbednar <xbedna60@fit.vutbr.cz>
2020-05-05 12:34:32 +0200

    Remove using Chrome profile during testing.

--- tests/integration_tests/configuration.py
+++ tests/integration_tests/configuration.py
@@ -8,6 +8,5 @@ class Config:
     firefox_profile = "C:\\Users\\Martin\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\voxsqf3a.default-esr"
     firefox_jsr_extension = "D:\\Development\\jsrestrictor\\tests\\common_files\\JSR\\firefox\\firefox_JSR_master.xpi"
     chrome_driver = "D:\\Development\\jsrestrictor\\tests\\common_files\\webbrowser_drivers\\chromedriver.exe"
-    chrome_profile = "C:\\Users\\Martin\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\voxsqf3a.default-esr"
     chrome_jsr_extension = "D:\\Development\\jsrestrictor\\tests\\common_files\\JSR\\chrome\\chrome_JSR_master.crx"
     testing_page = "https://polcak.github.io/jsrestrictor/test/test.html"
