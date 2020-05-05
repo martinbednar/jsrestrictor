b035db6ee6284288956d1a8b438fdccfbea1ce7a
martinbednar <xbedna60@fit.vutbr.cz>
2020-05-05 20:15:52 +0200

    Add comments.

--- tests/integration_tests/conftest.py
+++ tests/integration_tests/conftest.py
@@ -1,13 +1,21 @@
 import pytest
+
 from web_browser_shared import get_shared_browser
 import values_expected
 
 
+## This module is automatically called by pytest before start executing tests.
+#
+#  Here is defined variables browser and expected that are given to tests as a parameter.
+
+## browser provide shared browser to all tests
 @pytest.fixture(scope="session", autouse=True)
 def browser():
     return get_shared_browser()
 
-
+## expected provide expected values to all tests
+#
+#  expected values are selected based on testing jsr_level.
 @pytest.fixture(scope="session", autouse=True)
 def expected():
     if get_shared_browser().jsr_level == 0:
