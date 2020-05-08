87bda1c23ac0e7b9092e0da31f2d3a832dca87b7
martinbednar <xbedna60@fit.vutbr.cz>
2020-05-08 11:22:25 +0200

    CHange format of testing text output.

--- tests/integration_tests/start.py
+++ tests/integration_tests/start.py
@@ -1,4 +1,5 @@
 import pytest
+import output
 
 from web_browser import Browser
 from web_browser_type import BrowserType
@@ -8,28 +9,26 @@ from configuration import Config
 
 ## Main module - it starts and control testing.
 #
-#  To start testing call this module from PowerShell, ComendPrompt, Terminal or Bash: python start.py
+#  To start testing call this module from PowerShell, CommandPrompt, Terminal or Bash: python start.py
 #  For every browser and for every jsr_level defined in configuration.py set of all tests is run.
-#
-#  For every browser from configuration.py run set of test.
-for browser_type in Config.tested_browsers:
-    # create new browser of given type (Chrome, Firefox, etc.)
-    my_browser = Browser(browser_type)
-    # set current browser as shared browser for all testing levels (do not create new browser for every JSR_level)
-    set_shared_browser(my_browser)
-    # for every browser from configuration.py run set of test.
-    for jsr_level in Config.tested_jsr_levels:
-        print("--------------------------------------------------------------------------")
-        print("--------------------------------------------------------------------------")
-        print(">>>>>>>>>>>>>>>  TESTING <" + browser_type.name + ", JSR level = " + str(jsr_level) + "> STARTED   <<<<<<<<<<<<<<")
-        print()
-        # set jsr_level to given level
-        my_browser.jsr_level = jsr_level
-        # run set of tests
-        pytest.main(['./tests_definition/test_gps.py', '-s'])
-        print()
-        print(">>>>>>>>>>>>>>>  TESTING <" + browser_type.name + ", JSR level = " + str(jsr_level) + "> FINISHED  <<<<<<<<<<<<<<")
-        print("--------------------------------------------------------------------------")
-        print("--------------------------------------------------------------------------")
-    # Close browser.
-    my_browser.quit()
+def main():
+    #  For every browser from configuration.py run set of test.
+    for browser_type in Config.tested_browsers:
+        # create new browser of given type (Chrome, Firefox, etc.)
+        my_browser = Browser(browser_type)
+        # set current browser as shared browser for all testing levels (do not create new browser for every JSR_level)
+        set_shared_browser(my_browser)
+        # for every browser from configuration.py run set of test.
+        for jsr_level in Config.tested_jsr_levels:
+            output.print_testing_header(browser_type, jsr_level)
+            # set jsr_level to given level
+            my_browser.jsr_level = jsr_level
+            # run set of tests
+            pytest.main()
+            output.print_testing_footer(browser_type, jsr_level)
+        # Close browser.
+        my_browser.quit()
+
+
+if __name__ == "__main__":
+    main()
