5ad9a600f447121ce35d5dc51b710feb4fbfd55d
martinbednar <martin.bedn20@gmail.com>
2020-06-26 15:52:12 +0200

    Change sequential processing to parallel.

--- tests/system_tests/start.py
+++ tests/system_tests/start.py
@@ -1,4 +1,4 @@
-import subprocess
+from subprocess import Popen
 import time
 import csv
 import numpy as np
@@ -8,7 +8,7 @@ def read_n_top_sites_csv(n):
     with open('tranco.csv', 'r') as csvTopSites:
         data = np.array(list(csv.reader(csvTopSites)))
 
-    return data[1:n, 1]
+    return data[0:n, 1]
 
 
 
@@ -16,25 +16,32 @@ start_server = ['java', '-jar', 'selenium-server-standalone-3.141.59.jar', '-rol
 start_node1 = ['java', '-Dwebdriver.gecko.driver=D:\\Development\\jsrestrictor\\tests\\common_files\\webbrowser_drivers\\geckodriver.exe', '-Dwebdriver.chrome.driver=D:\\Development\\jsrestrictor\\tests\\common_files\\webbrowser_drivers\\chromedriver.exe', '-jar', 'selenium-server-standalone-3.141.59.jar', '-role', 'node', '-hub', 'https://localhost:4444/grid/register/']
 start_node2 = ['java', '-Dwebdriver.gecko.driver=D:\\Development\\jsrestrictor\\tests\\common_files\\webbrowser_drivers\\geckodriver.exe', '-Dwebdriver.chrome.driver=D:\\Development\\jsrestrictor\\tests\\common_files\\webbrowser_drivers\\chromedriver.exe', '-jar', 'selenium-server-standalone-3.141.59.jar', '-role', 'node', '-hub', 'https://localhost:4444/grid/register/']
 
-server = subprocess.Popen(start_server)
+server = Popen(start_server)
 print(server)
 
 time.sleep( 5 )
 
-node1 = subprocess.Popen(start_node1)
+node1 = Popen(start_node1)
 print(node1)
 time.sleep( 7 )
-node2 = subprocess.Popen(start_node2)
+node2 = Popen(start_node2)
 print(node2)
 
 time.sleep( 7 )
 
 ####################################
 try:
+    processes = []
+
     for top_site in read_n_top_sites_csv(n=3):
         run_test = ['python', 'test.py', top_site]
-        subprocess.run(run_test)
+        processes.append(Popen(run_test, shell=True))
         time.sleep(5)
+
+    i = 0
+    for process in processes:
+        processes[i].wait()
+        i+=1
 
 ###################################
 
