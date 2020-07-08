10d86408ce85c3ff527bd125ef04d61cbed56b27
martinbednar <martin.bedn20@gmail.com>
2020-07-08 20:56:54 +0200

    Add properities to Configuration.

--- tests/system_tests/get_data/configuration.py
+++ tests/system_tests/get_data/configuration.py
@@ -1,7 +1,17 @@
-﻿class MetaConfig(type):
+﻿from web_browser_type import BrowserType
+from test_type import TestType
+
+
+class MetaConfig(type):
+    @property
+    def tested_browsers(self):
+        return self._tested_browsers
     @property
     def jsr_level(self):
         return self._jsr_level
+    @property
+    def perform_tests(self):
+        return self._perform_tests
     @property
     def number_of_sites_for_testing(self):
         return self._number_of_sites_for_testing
@@ -20,7 +30,9 @@
 
 
 class Config(metaclass=MetaConfig):
+    _tested_browsers = [BrowserType.CHROME]
     _jsr_level = 3
+    _perform_tests = [TestType.LOGS, TestType.SCREENSHOTS]
     _number_of_sites_for_testing = 10
     _number_of_grid_nodes = 1
     #max is number_of_grid_nodes * 5
