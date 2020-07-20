ef7fb9a98c8944da13a57e04edb1ff18ac559750
martinbednar <martin.bedn20@gmail.com>
2020-07-20 19:24:46 +0200

    Update levels unit tests according to source code changes.

--- tests/unit_tests/tests/common/levels_tests.js
+++ tests/unit_tests/tests/common/levels_tests.js
@@ -1,7 +1,7 @@
 /// <reference path="../../common/levels.js">
 
 describe("Levels", function() {
-	describe("getCurrentLevelJSON function", function() {
+	describe("Function getCurrentLevelJSON", function() {
 		beforeAll(function() {
 			domains = {};
 			domains['stackoverflow.com'] = level_3;
@@ -40,21 +40,20 @@ describe("Levels", function() {
 			expect(function() {getCurrentLevelJSON("www.seznam.cz")}).toThrowError();
 		});
 		it("should return default level when root domain from given URL is not saved in domains in browser storage",function() {
-			expect(getCurrentLevelJSON("https://www.seznam.cz/")).toBe(default_level);
-			expect(getCurrentLevelJSON("https://www.fit.vut.cz/research/groups/.cs")).toBe(default_level);
+			expect(getCurrentLevelJSON("https://www.seznam.cz/")[0]).toBe(default_level);
+			expect(getCurrentLevelJSON("https://www.fit.vut.cz/research/groups/.cs")[0]).toBe(default_level);
 		});
 		it("should return set level (from browser storage) for saved domains",function() {
-			expect(getCurrentLevelJSON("https://stackoverflow.com/questions/1925976/declaring-functions-in-javascript")).toBe(level_3);
-			expect(getCurrentLevelJSON("http://www.example.net/?amount=brass&bird=basketball")).toBe(level_3);
-			expect(getCurrentLevelJSON("https://www.vas-hosting.cz/blog-vyhody-dedikovaneho-serveru-vds-oproti-vps")).toBe(level_2);
-			expect(getCurrentLevelJSON("https://www.csob.cz/portal/lide")).toBe(level_0);
+			expect(getCurrentLevelJSON("https://stackoverflow.com/questions/1925976/declaring-functions-in-javascript")[0]).toBe(level_3);
+			expect(getCurrentLevelJSON("http://www.example.net/?amount=brass&bird=basketball")[0]).toBe(level_3);
+			expect(getCurrentLevelJSON("https://www.vas-hosting.cz/blog-vyhody-dedikovaneho-serveru-vds-oproti-vps")[0]).toBe(level_2);
+			expect(getCurrentLevelJSON("https://www.csob.cz/portal/lide")[0]).toBe(level_0);
 		});
-		// Now this test is not passing because of bug in url - look at url_tests for more information.
-		xit("should return set level (from browser storage) for saved domains with subdomains",function() {
-			expect(getCurrentLevelJSON("https://polcak.github.io/jsrestrictor/test/test.html")).toBe(level_2);
-			expect(getCurrentLevelJSON("https://swatblog.rtgp.xyz/")).toBe(level_1);
-			expect(getCurrentLevelJSON("https://mail.google.com/mail/u/0/#inbox")).toBe(level_0);
-			expect(getCurrentLevelJSON("https://thenetworg.crm4.dynamics.com/main.aspx#759240725")).toBe(level_1);
+		it("should return set level (from browser storage) for saved domains with subdomains",function() {
+			expect(getCurrentLevelJSON("https://polcak.github.io/jsrestrictor/test/test.html")[0]).toBe(level_2);
+			expect(getCurrentLevelJSON("https://swatblog.rtgp.xyz/")[0]).toBe(level_1);
+			expect(getCurrentLevelJSON("https://mail.google.com/mail/u/0/#inbox")[0]).toBe(level_0);
+			expect(getCurrentLevelJSON("https://thenetworg.crm4.dynamics.com/main.aspx#759240725")[0]).toBe(level_1);
 		});
 	});
 });
