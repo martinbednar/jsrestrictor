d968dc77f3ec449cc31eee465c4f97f13f7a34e2
martinbednar <martin.bedn20@gmail.com>
2020-07-20 20:12:17 +0200

    Remove old Node.js unit tests.

--- tests/unit_tests_old/test/global_setup.js
+++
@@ -1,9 +0,0 @@
-const { default: browserFake } = require('webextensions-api-fake');
-global.browser = browserFake();
-
-global.chai = require("chai");
-global.asserttype = require('chai-asserttype');
-chai.use(asserttype);
-global.expect = chai.expect;
-
-global.rewire = require('rewire');
