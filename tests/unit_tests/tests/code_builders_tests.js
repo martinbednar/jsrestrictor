1bcdfa6d0f274f4c407eda2a712cb840027adca9
martinbednar <martin.bedn20@gmail.com>
2020-07-24 18:06:42 +0200

    Restructuralize unit tests.

---
+++ tests/unit_tests/tests/code_builders_tests.js
@@ -0,0 +1,9 @@
+/// <reference path="../../common/code_builders.js">
+
+describe("Code builders", function() {
+	describe("Function enclose_wrapping", function() {		
+		it("should be defined",function() {
+			expect(enclose_wrapping).toBeDefined();
+		});
+	});
+});
