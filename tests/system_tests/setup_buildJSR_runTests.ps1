f78622e920af4e107f7f46658c23d6a464190f95
martinbednar <martin.bedn20@gmail.com>
2020-07-28 09:14:22 +0200

    Add scripts for automatically running system tests.

---
+++ tests/system_tests/setup_buildJSR_runTests.ps1
@@ -0,0 +1,24 @@
+$Error.clear()
+
+# Go to common scripts directory.
+cd ..\common_files\scripts
+
+# Run script build_JSR_package.ps1
+.\build_JSR_package.ps1
+
+# Go back to system_tests directory.
+cd ..\..\system_tests
+
+# Start testing if everything ok.
+Write-Host
+# Handle errors.
+if($Error.length -gt 0)
+{
+	Write-Host "An error noticed during setup the test environment. Integration testing can not be started. Look at the README file and follow instructions to run the setup again."
+}
+else {
+	Write-Host "No error noticed during setup the test environment. Integration testing is starting..."
+	python ./get_data/start.py
+	python ./analyze_data/start_screenshots_analysis.py
+	python ./analyze_data/start_logs_analysis.py
+}
---
+++ tests/system_tests/setup_buildJSR_runTests.sh
@@ -0,0 +1,36 @@
+# Handle errors.
+# Function called by trap before exit caused by error.
+function beforeExit {
+	if [ $retVal -ne 0 ]; then
+		echo "\"${last_command}\" command failed with exit code $?."
+		echo
+		echo "An error noticed during setup the test environment. Integration testing can not be started. Look at the README file and follow instructions to run the setup again."
+	fi
+	exit $retVal
+}
+# exit when any command fails
+set -euo pipefail
+# Call function before exit caused by error.
+trap beforeExit EXIT
+
+# Go to common scripts directory.
+cd ../common_files/scripts
+
+# Set executable permissions.
+chmod +x ./build_JSR_package.sh
+chmod +x ../webbrowser_drivers/chromedriver
+
+# Run script build_JSR_package.sh
+./build_JSR_package.sh
+
+# Go back to integration_tests directory.
+cd ../../system_tests
+
+# Start testing if everything ok.
+# Stop handling errors.
+set +euo pipefail
+echo
+echo "No error noticed during setup the test environment. Integration testing is starting..."
+python3 ./get_data/start.py
+python3 ./analyze_data/start_screenshots_analysis.py
+python3 ./analyze_data/start_logs_analysis.py
