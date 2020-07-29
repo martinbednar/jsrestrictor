67ebe7c152039b4a22c723c1ee3902ad18ae4e5c
martinbednar <martin.bedn20@gmail.com>
2020-07-29 07:37:38 +0200

    Finish unit tests for code_builder

--- tests/unit_tests/tests/code_builders_tests.js
+++ tests/unit_tests/tests/code_builders_tests.js
@@ -1,69 +1,7 @@
 /// <reference path="../../common/code_builders.js">
 
 describe("Code builders", function() {
-	describe("Function enclose_wrapping", function() {
-		it("should be defined",function() {
-			expect(enclose_wrapping).toBeDefined();
-		});
-	});
-	describe("Function enclose_wrapping2", function() {
-		it("should be defined",function() {
-			expect(enclose_wrapping2).toBeDefined();
-		});
-	});
-	describe("Function define_page_context_function", function() {
-		it("should be defined",function() {
-			expect(define_page_context_function).toBeDefined();
-		});
-	});
-	describe("Function generate_assign_function_code", function() {
-		it("should be defined",function() {
-			expect(generate_assign_function_code).toBeDefined();
-		});
-	});
-	describe("Function generate_object_properties", function() {
-		it("should be defined",function() {
-			expect(generate_object_properties).toBeDefined();
-		});
-	});
 	describe("Function build_code", function() {
-		beforeAll(function() {
-			my_very_simple_wrapper = {
-				wrapped_objects: [],
-				parent_object: "window",
-				parent_object_property: "Float64Array"
-			};
-			my_simple_wrapper = {
-				wrapped_objects: [],
-				parent_object: "window",
-				parent_object_property: "Float64Array",
-				post_wrapping_code: [{
-					code_type: "object_properties",
-					parent_object: "navigator",
-					parent_object_property: "deviceMemory",
-					wrapped_objects: [],
-					wrapped_properties: [{
-						property_name: "get",
-						property_value: "function(){return 4;}"
-					}]
-				}]
-			};
-			my_complicated_wrapper = {
-				wrapped_objects: [],
-				parent_object: "window",
-				parent_object_property: "Float64Array",
-				post_wrapping_code: [{
-					code_type: "object_properties",
-					parent_object: "navigator",
-					parent_object_property: "deviceMemory",
-					wrapped_objects: [],
-					wrapped_properties: [{
-						property_name: "get",
-						property_value: "function(){return 4;}"
-					}]
-				}]
-			};
-		});
 		it("should be defined",function() {
 			expect(build_code).toBeDefined();
 		});
@@ -71,12 +9,37 @@ describe("Code builders", function() {
 			expect(function() {build_code()}).toThrowError();
 		});
 		it("should return string",function() {
+			var my_very_simple_wrapper = {
+				wrapped_objects: [],
+				parent_object: "window",
+				parent_object_property: "Float64Array"
+			};
 			expect(build_code(my_very_simple_wrapper)).toEqual(jasmine.any(String));
 		});
 		it("should return right code when argument is very simple wrapper",function() {
+			var my_very_simple_wrapper = {
+				wrapped_objects: [],
+				parent_object: "window",
+				parent_object_property: "Float64Array"
+			};
 			expect(build_code(my_very_simple_wrapper)).toBe("(function(...args) {Object.freeze(window.Float64Array);})();");
 		});
 		it("should return right code when argument is simple wrapper",function() {
+			var my_simple_wrapper = {
+				wrapped_objects: [],
+				parent_object: "window",
+				parent_object_property: "Float64Array",
+				post_wrapping_code: [{
+					code_type: "object_properties",
+					parent_object: "navigator",
+					parent_object_property: "deviceMemory",
+					wrapped_objects: [],
+					wrapped_properties: [{
+						property_name: "get",
+						property_value: "function(){return 4;}"
+					}]
+				}]
+			};
 			expect(build_code(my_simple_wrapper)).toBe(`(function(...args) {
 		if (!("deviceMemory" in navigator)) {
 			// Do not wrap an object that is not defined, e.g. because it is experimental feature.
@@ -104,9 +67,59 @@ describe("Code builders", function() {
 		"deviceMemory", descriptor);
 	Object.freeze(window.Float64Array);})();`);
 		});
-	});
-	it("should return right code when argument is complicated wrapper",function() {
-		expect(build_code(my_very_simple_wrapper)).toBe("(function(...args) {Object.freeze(window.Float64Array);})();");
+		it("should return right code when argument is complicated wrapper",function() {
+			var my_complicated_wrapper = {
+				wrapped_objects: [{
+					original_name: "XMLHttpRequest",
+					wrapped_name: "originalXMLHttpRequest"
+				}],
+				parent_object: "window",
+				parent_object_property: "Float64Array",
+				post_wrapping_code: [{
+					code_type: "object_properties",
+					parent_object: "navigator",
+					parent_object_property: "deviceMemory",
+					wrapped_objects: [],
+					wrapped_properties: [{
+						property_name: "get",
+						property_value: "function(){return 4;}"
+					}]
+				}]
+			};
+			expect(build_code(my_complicated_wrapper)).toBe(`(function(...args) {
+			var originalXMLHttpRequest = XMLHttpRequest;
+			if (originalXMLHttpRequest === undefined) {
+				// Do not wrap an object that is not defined, e.g. because it is experimental feature.
+				// This should reduce fingerprintability.
+				return;
+			}
+		
+		if (!("deviceMemory" in navigator)) {
+			// Do not wrap an object that is not defined, e.g. because it is experimental feature.
+			// This should reduce fingerprintability.
+			return;
+		}
+	descriptor = Object.getOwnPropertyDescriptor(
+			navigator, "deviceMemory");
+		if (descriptor === undefined) {
+			descriptor = { // Originally not a descriptor
+				get: navigator.deviceMemory,
+				set: undefined,
+				configurable: false,
+				enumerable: true,
+			};
+		}
+	
+			originalPDF = descriptor["get"];
+			replacementPD = function(){return 4;};
+			descriptor["get"] = replacementPD;
+			if (replacementPD instanceof Function) {
+				original_functions[replacementPD.toString()] = originalPDF.toString();
+			}
+		Object.defineProperty(navigator,
+		"deviceMemory", descriptor);
+	Object.freeze(window.Float64Array);})();`);
+		});
 	});
 	describe("Function wrap_code", function() {
 		beforeAll(function() {
